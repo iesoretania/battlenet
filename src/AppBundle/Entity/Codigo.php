cff45f9f71039ee3f85b051f1ea8414007cf8058
Luis Ramón López <lrlopez@gmail.com>
2016-02-13 00:00:25 +0100

    Añadida entidad Codigo para controlar los registros

---
+++ src/AppBundle/Entity/Codigo.php
@@ -0,0 +1,165 @@
+<?php
+/*
+  Battlenet
+
+  Copyright (C) 2016: Luis Ramón López López
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see [http://www.gnu.org/licenses/].
+*/
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Validator;
+
+/**
+ * @ORM\Entity()
+ */
+class Codigo
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $token;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $fechaGeneracion;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var \DateTime
+     */
+    private $fechaRegistro;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Equipo")
+     * @ORM\JoinColumn(nullable=true)
+     * @var Equipo
+     */
+    private $equipo;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     * @return Codigo
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string 
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set fechaGeneracion
+     *
+     * @param \DateTime $fechaGeneracion
+     * @return Codigo
+     */
+    public function setFechaGeneracion($fechaGeneracion)
+    {
+        $this->fechaGeneracion = $fechaGeneracion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaGeneracion
+     *
+     * @return \DateTime 
+     */
+    public function getFechaGeneracion()
+    {
+        return $this->fechaGeneracion;
+    }
+
+    /**
+     * Set fechaRegistro
+     *
+     * @param \DateTime $fechaRegistro
+     * @return Codigo
+     */
+    public function setFechaRegistro($fechaRegistro)
+    {
+        $this->fechaRegistro = $fechaRegistro;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaRegistro
+     *
+     * @return \DateTime 
+     */
+    public function getFechaRegistro()
+    {
+        return $this->fechaRegistro;
+    }
+
+    /**
+     * Set equipo
+     *
+     * @param Equipo $equipo
+     * @return Codigo
+     */
+    public function setEquipo(Equipo $equipo = null)
+    {
+        $this->equipo = $equipo;
+
+        return $this;
+    }
+
+    /**
+     * Get equipo
+     *
+     * @return Equipo
+     */
+    public function getEquipo()
+    {
+        return $this->equipo;
+    }
+}
