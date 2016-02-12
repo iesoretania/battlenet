d6404372473f43df046b1163cd5382d83ac7952a
Luis Ramón López <lrlopez@gmail.com>
2016-02-12 23:34:15 +0100

    Añadido formulario de Anotación

---
+++ src/AppBundle/Form/Type/AnotacionType.php
@@ -0,0 +1,59 @@
+<?php
+/*
+  Battlenet
+
+  Copyright (C) 2016: Luis Ramón López López
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see [http://www.gnu.org/licenses/].
+*/
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnotacionType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fechaHora', null, [
+                'label' => 'Fecha y hora'
+            ])
+            ->add('equipo', null, [
+                'label' => 'Equipo'
+            ])
+            ->add('concepto', null, [
+                'label' => 'Concepto'
+            ])
+            ->add('puntuacion', 'Symfony\Component\Form\Extension\Core\Type\IntegerType', [
+                'label' => 'Puntuación'
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Anotacion'
+        ]);
+    }
+}
