fe92e94634fb3062f1a8bbb36327b16068bdeac0
Luis Ramón López <lrlopez@gmail.com>
2016-02-12 23:46:39 +0100

    Primer fixture: datos iniciales

---
+++ src/AppBundle/DataFixtures/ORM/InicialData.php
@@ -0,0 +1,57 @@
+<?php
+/*
+  Battlenet
+
+  Copyright (C) 2016: Luis Ramón López López
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see [http://www.gnu.org/licenses/].
+*/
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Estado;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class InicialData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $estados = [
+            ['regi', 'Registrado'],
+            ['fas1', 'Construyendo base'],
+            ['fas2', 'Atacando bases enemigas'],
+            ['fina', 'Finalizado'],
+            ['desc', 'Descalificado']
+        ];
+
+        foreach($estados as $it => $datos) {
+            $estado = new Estado();
+            $estado
+                ->setId($datos[0])
+                ->setDescripcion($datos[1])
+                ->setOrden($it);
+
+            $manager->persist($estado);
+        }
+
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+}
